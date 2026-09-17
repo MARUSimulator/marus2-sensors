985db259e5f2bdfd91547e6514308c8583cdcb71
natkokrasevac <natko.krasevac@fer.hr>
2026-09-17 11:02:14 +0200

    Improve CameraSensor with double-buffered AsyncGPUReadback and configurable resolution

--- Runtime/RGB/CameraSensor.cs
+++ Runtime/RGB/CameraSensor.cs
@@ -27,60 +27,137 @@ namespace Marus.Sensors
     /// </summary>
     public class CameraSensor : SensorBase
     {
-        [ReadOnly]
+        [Header("Image Resolution")]
+        [Tooltip("Horizontal resolution in pixels")]
         public int ImageWidth = 1920;
 
-        [ReadOnly]
+        [Tooltip("Vertical resolution in pixels")]
         public int ImageHeight = 1080;
 
+        [Tooltip("If true, overrides ImageWidth and ImageHeight with the current screen/game view resolution.")]
+        public bool matchScreenResolution = false;
+
         Camera _camera;
-        RenderTexture _tmpTexture;
-        TextureFormat _textureFormat = TextureFormat.RGB24;
-        Texture2D _texture;
+        RenderTexture _renderTexture;
+        readonly TextureFormat _textureFormat = TextureFormat.RGB24;
+
+        byte[] _frontBuffer;
+        byte[] _backBuffer;
+        readonly object _bufferLock = new object();
 
         [HideInInspector]
         public byte[] Data;
 
         void Start()
         {
-
+            if (matchScreenResolution)
+            {
 #if UNITY_EDITOR
-            string[] res = UnityStats.screenRes.Split('x');
-            ImageWidth =  int.Parse(res[0]);
-            ImageHeight = int.Parse(res[1]);
+                string[] res = UnityStats.screenRes.Split('x');
+                if (res.Length >= 2 && int.TryParse(res[0], out int w) && int.TryParse(res[1], out int h))
+                {
+                    ImageWidth = w;
+                    ImageHeight = h;
+                }
 #else
-            ImageWidth =  Screen.width;
-            ImageHeight = Screen.height;
+                ImageWidth = Screen.width;
+                ImageHeight = Screen.height;
 #endif
+            }
 
             _camera = GetComponent<Camera>();
             _camera.enabled = false;
-            Data = new byte[ImageHeight*ImageWidth*3];
-            _texture = new Texture2D
-            (
-                ImageWidth,
-                ImageHeight,
-                _textureFormat,
-                false
-            );
+
+            InitializeBuffers();
+        }
+
+        private void InitializeBuffers()
+        {
+            if (_renderTexture != null)
+            {
+                if (_camera != null && _camera.targetTexture == _renderTexture)
+                {
+                    _camera.targetTexture = null;
+                }
+                _renderTexture.Release();
+                Destroy(_renderTexture);
+                _renderTexture = null;
+            }
+
+            _renderTexture = new RenderTexture(ImageWidth, ImageHeight, 16, RenderTextureFormat.ARGB32)
+            {
+                name = $"{gameObject.name}_CameraSensorRT",
+                filterMode = FilterMode.Bilinear,
+                wrapMode = TextureWrapMode.Clamp
+            };
+            _renderTexture.Create();
+            _camera.targetTexture = _renderTexture;
+
+            int byteSize = ImageWidth * ImageHeight * 3;
+            lock (_bufferLock)
+            {
+                _frontBuffer = new byte[byteSize];
+                _backBuffer = new byte[byteSize];
+                Data = _frontBuffer;
+            }
         }
 
         protected override void SampleSensor()
         {
-            if (_camera.targetTexture != null)
+            if (_renderTexture == null || _renderTexture.width != ImageWidth || _renderTexture.height != ImageHeight)
             {
-                RenderTexture.ReleaseTemporary(_camera.targetTexture);
+                InitializeBuffers();
             }
-            _camera.targetTexture = RenderTexture.GetTemporary(ImageWidth, ImageHeight, 16);
-            RenderTexture.active = _camera.targetTexture;
+
             _camera.Render();
-            AsyncGPUReadback.Request(_camera.targetTexture, 0, _textureFormat, ReadbackCompleted);
+            AsyncGPUReadback.Request(_renderTexture, 0, _textureFormat, ReadbackCompleted);
         }
 
         void ReadbackCompleted(AsyncGPUReadbackRequest request)
         {
-            Data = request.GetData<byte>().ToArray();
-            hasData = true;
+            if (request.hasError)
+            {
+                Debug.LogWarning($"[CameraSensor] AsyncGPUReadback error on {gameObject.name}");
+                return;
+            }
+
+            var nativeData = request.GetData<byte>();
+            int expectedSize = ImageWidth * ImageHeight * 3;
+            if (nativeData.Length != expectedSize)
+            {
+                return;
+            }
+
+            lock (_bufferLock)
+            {
+                if (_backBuffer == null || _backBuffer.Length != expectedSize)
+                {
+                    _backBuffer = new byte[expectedSize];
+                }
+
+                nativeData.CopyTo(_backBuffer);
+
+                // Double buffer swap: front buffer is exposed via Data for zero-allocation reading
+                var temp = _frontBuffer;
+                _frontBuffer = _backBuffer;
+                _backBuffer = temp;
+                Data = _frontBuffer;
+                hasData = true;
+            }
+        }
+
+        void OnDestroy()
+        {
+            if (_camera != null && _camera.targetTexture == _renderTexture)
+            {
+                _camera.targetTexture = null;
+            }
+            if (_renderTexture != null)
+            {
+                _renderTexture.Release();
+                Destroy(_renderTexture);
+                _renderTexture = null;
+            }
         }
     }
 }
